f184adf7ff65872f02fa116e264001ab1969cd3c
himax82 <himax82@gmail.com>
2021-11-01 20:37:15 +0800

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("user", "Petr Arsentev");
+        List<String> names = List.of("Витя", "Игорь", "Михаил", "Сергей");
+        model.addAttribute("names", names);
         return "index";
     }
 }
