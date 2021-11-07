03a201843b58a8b6f05fc5cde4faf056525e141d
himax82 <himax82@gmail.com>
2021-11-07 20:22:16 +0800

    2. Регистрация пользователя [#296069 #225274]

--- src/main/java/ru/job4j/accident/controller/RegControl.java
+++ src/main/java/ru/job4j/accident/controller/RegControl.java
@@ -2,6 +2,7 @@ package ru.job4j.accident.controller;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,12 +24,18 @@ public class RegControl {
     }
 
     @PostMapping("/reg")
-    public String regSave(@ModelAttribute User user) {
-        user.setEnabled(true);
-        user.setPassword(encoder.encode(user.getPassword()));
-        user.setAuthority(authorities.findByAuthority("ROLE_USER"));
-        users.save(user);
-        return "redirect:/login";
+    public String regSave(@ModelAttribute User user, Model model) {
+        if (users.findByUsername(user.getUsername()).isEmpty()) {
+            user.setEnabled(true);
+            user.setPassword(encoder.encode(user.getPassword()));
+            user.setAuthority(authorities.findByAuthority("ROLE_USER"));
+            users.save(user);
+            return "redirect:/login";
+        }
+        else {
+            model.addAttribute("errorMessage", "Такой пользователь уже существует!");
+            return "/reg";
+        }
     }
 
     @GetMapping("/reg")
--- src/main/webapp/WEB-INF/views/reg.jsp
+++ src/main/webapp/WEB-INF/views/reg.jsp
@@ -13,6 +13,11 @@
     <title>Регистрация</title>
 </head>
 <body>
+<c:if test="${not empty errorMessage}">
+    <div style="color:red; font-weight: bold; margin: 30px 0px;">
+            ${errorMessage}
+    </div>
+</c:if>
 <div class="container pt-3">
     <div class="row">
         <div class="card" style="width: 100%">
